a59d045410c68c08d21fb054993ffcabfe8171bb
Robert <robert.bezem@gmail.com>
2016-11-07 09:21:01 +0100

    moved python Reader to seperate folder and added python protobuf data

--- src/project/reader/pythonReader.py
+++ src/project/reader/pythonReader.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 import serial
 import sys
+import game_pb2
 x = serial.Serial(port='/dev/ttyACM0',baudrate=38400,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS)
-print >> sys.stderr, 'Press enter to continue'
-raw_input()
-x.write("\n".encode())
 
+raw_input("Press enter to retrieve data\n")
+x.write("\n")
 length = x.readline()
 length = int(length)
-print >> sys.stderr, length
+data = ''
 lengthalreadyread = 0
 while lengthalreadyread < length:
-	sys.stdout.write(x.read())
+	data = data + x.read()
 	lengthalreadyread = lengthalreadyread + 1
+thingz = game_pb2.GameData()
+thingz.ParseFromString(data)
+print thingz
