44ffc845b7b473d18f25fdc63b0ec69d2fbe2418
LuukSteeman <luuksteeman@live.nl>
2016-11-08 13:34:02 +0100

    Fixed deadlock issue, activated shootctrl with keypad

--- src/project/main.cpp
+++ src/project/main.cpp
@@ -4,35 +4,22 @@
 #include "controllers/shootCtrl.hpp"
 #include "boundary/transmitter.hpp"
 #include "controllers/transmitterctrl.hpp"
+#include "boundary/keypad.hpp"
+#include "controllers/keypadHandler.hpp"
 
-class Main : rtos::task<>
-{
-  shootCtrl & shoot;
-
-private:
-  void main()
-  {
-    while (1)
-    {
-      // sleep(10 * rtos::s);
-      shoot.keyPressed('*');
-      sleep(2 * rtos::s);
-    }
-  }
-
-public:
-  Main(shootCtrl &shoot, char *name) : task(name),
-                                                shoot(shoot){};
-};
 
 int main()
 {
   WDT->WDT_MR = WDT_MR_WDDIS;
   hwlib::wait_ms(500);
 
+  Keypad pad;
   Transmitter trans;
   Transmitterctrl transctrl(trans);
   shootCtrl shoot((char*)"task for shooting", transctrl);
+  pad.addKeypadListener(&shoot);
+  KeypadHandler handler(pad);
+
   rtos::run();
 
   return 0;
