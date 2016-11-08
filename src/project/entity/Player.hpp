a87f850c4d177378ba4f64a9af905213e2545d2d
Vallkyr <ricardo.bouwman@outlook.com>
2016-11-08 12:01:08 +0100

    Player entity made

---
+++ src/project/entity/Player.hpp
@@ -0,0 +1,30 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+//! Player Entity Class
+
+class Player
+{
+private:
+	//! The amount of health the player has, starting at 100
+	int hitPoints = 100;
+
+	//! The player's chosen weapon, which is used to calculate damage and rate of fire
+	int weaponID;
+public:
+	//! An empty constructor that doesn't set any values
+	Player(){};
+	
+	//! Update the player's health by subtracting the amount of damage taken
+	//! @param damage The amount of damage the player took
+	void updateHP(int damage){hitPoints -= damage;};
+
+	//! Set the player's weapon
+	//! @param The weapon ID chosen on the keypad
+	void setWeapon(int weapon){weaponID = weapon;};
+
+	//! Returns the ID of the player's chosen weapon
+	int getWeapon(){return weaponID;};
+};
+
+#endif // PLAYER_HPP
