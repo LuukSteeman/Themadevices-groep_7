848fef2fcb6b719c1a9a8b2b094d2e6f56caeaae
Jeroen <jeroenvanhattem@outlook.com>
2016-11-08 14:11:25 +0100

    Starting from scratch

---
+++ src/project/_main.cpp
@@ -0,0 +1,41 @@
+#include <hwlib.hpp>
+#include <rtos.hpp>
+
+#include "drawables/text.hpp"
+#include "controllers/oled_controller.hpp"
+#include "boundary/oled.hpp"
+
+
+class Main : rtos::task<> {
+
+
+private:
+	oled_buffered_d18_d21	 oled;
+	oled_controller oled_control((char*)"OLED Controller Task");
+	text t(&oled_control, 2, (char*)"Hi");
+	void main() {
+		while (1) {
+			t.draw();
+			sleep(2 * rtos::s);
+		};
+	};
+
+public:
+	Main(oled_controller & oled_control, char *name) : 
+		oled_control(oled_control), task(name)
+	{};
+};
+
+
+int main()
+{
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	hwlib::wait_ms(500);
+
+	auto oled_control = oled_controller((char*)"Controller");
+	auto Maintask = Main(&oled_control, "Testmaintask");
+
+	rtos::run();
+
+	return 0;
+}
--- src/project/boundary/oled.cpp
+++
@@ -1,1 +0,0 @@
-#include "oled.hpp"
