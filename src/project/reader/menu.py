b9cc3216e9d9cc7760477a61bc5233f81e04f610
Robert <robert.bezem@gmail.com>
2016-11-09 08:36:40 +0100

    checks and sort

--- src/project/reader/menu.py
+++ src/project/reader/menu.py
@@ -4,8 +4,11 @@ def showMenu(menus, question='Please select a option:'):
     for i in range(0,len(menus)):
         strr += str(i+1) + ":" + menus[i][0] + "\n"
     x = raw_input(strr)
-    x = int(x)-1
-    if(x < len(menus) and x > -1):
-        menus[x][1]()
-    else:
-        showMenu(menus)
+    try:
+        x = int(x)-1
+    finally:
+        if(type(x) is int and x < len(menus) and x > -1):
+             menus[x][1]()
+        else:
+            showMenu(menus, question=question+'\ninvalid input')
+        
